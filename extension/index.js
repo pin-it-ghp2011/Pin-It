a75d26463fd3152298efa17c4a4d2c20fe7ee420
yqivy <yqivy.liu@gmail.com>
2021-02-08 13:01:34 -0500

    (feat): TRYING TO GET THIS TO WORK

---
+++ extension/index.js
@@ -0,0 +1,37 @@
+const head = document.head.outerHTML
+const img = document.querySelector('meta[property="og:image"][content]').content
+const title = document.getElementsByTagName('title')[0].innerHTML
+const aTags = document.getElementsByTagName('a')
+// $("a").remove()
+
+const articles = document.getElementsByTagName('article')
+const preBody = articles.length
+  ? articles[0].innerHTML
+  : `<html>${Array.from(document.getElementsByTagName('p')).reduce(
+      (str, elem) => str + '<p>' + elem.innerHTML + '</p>',
+      ''
+    )}</html>`
+
+const body = stripTags(preBody)
+
+const sendArticle = async () => {
+  await chrome.runtime.sendMessage({head, body, title, img}, function(
+    response
+  ) {})
+}
+
+sendArticle()
+
+// strips targeted HTML tags (hyperlinks, iframes and asides) from our body text
+function stripTags(str) {
+  let noAnchors = str.replace(/<a[^>]+>/g, '').replace(/<\/a>/g, '')
+
+  let noIframe = noAnchors
+    .replace(/<iframe[^>]+>/g, '')
+    .replace(/<\/iframe>/g, '')
+
+  return noIframe
+    .replace(/<aside[^>]+>/g, '')
+    .replace(/<aside>/g, '')
+    .replace(/<\/aside>/g, '')
+}
--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,62 @@
-// const pupperteer = require('puppeteer')
+const form = document.getElementById('loginform')
+const saveButton = document.getElementById('save-article')
+const name = document.getElementById('name')
+const logOut = document.getElementById('logOut')
+form.onsubmit = submit
+logOut.onclick = onLogOut
+
+if (JSON.parse(localStorage.getItem('user'))) {
+  form.style.display = 'none'
+  saveButton.style.display = 'block'
+  logOut.style.display = 'block'
+  name.innerHTML += JSON.parse(localStorage.getItem('user')).displayName
+}
+
+function onLogOut(event) {
+  event.preventDefault()
+  chrome.runtime.sendMessage({type: logOut.id}, function(response) {
+    form.style.display = 'block'
+    saveButton.style.display = 'none'
+    logOut.style.display = 'none'
+  })
+}
+
+function submit(event) {
+  event.preventDefault()
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+  chrome.runtime.sendMessage(
+    {type: form.id, email: email.value, password: password.value},
+    function(response) {
+      //user successfully logged in hide login form show article button
+      if (response === 'success') {
+        form.style.display = 'none'
+        saveButton.style.display = 'block'
+        logOut.style.display = 'block'
+      }
+    }
+  )
+}
+
+saveButton.addEventListener('click', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {file: 'index.js'})
+  })
+  alert('Your article is saved!')
+})
+
+// const pupperteer = require("puppeteer")
 
 // const url = window.location.href()
 
-// const saveButton = document.getElementsById('save-article')
+// const saveButton = document.getElementsById("save-article")
 
 // const puppeteerArticle = async () => {
 //   const browser = await puppeteer.launch()
 //   const page = await browser.newPage()
-//   await page.goto(url, {waitUntil: 'networkidle2'})
+//   await page.goto(url, {waitUntil: "networkidle2"})
 //   let title = await await page.title()
-//   await page.waitForSelector('body')
+//   await page.waitForSelector("body")
 //   const body = await page.evaluate(() => document.body.innerHTML)
 //   await browser.close()
 //   const article = {
@@ -21,4 +68,4 @@
 //   return article
 // }
 
-// saveButton.addEventListener('click', saveIt());
+// saveButton.addEventListener("click", saveIt());
